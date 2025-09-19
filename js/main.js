451981b14d01711c81239e168d9deaf1b9667f10
Brayan Estiven Andrade Carrillo <brayandrade33@gmail.com>
2025-09-18 20:28:00 -0500

    Se agrego la funcion de tareas terminadas, ademas de poder eliminarlas

--- js/main.js
+++ js/main.js
@@ -1,45 +1,118 @@
-//Creamos las consantes globales para los elementos principales 
-const input = document.getElementById("to-do");
-const addBtn = document.getElementById("add");
+
+//CREAMOS LAS CONSTANTES GLOBALES PARA LOS ELEMENTOS PRINCIPALES
+
+const input = document.getElementById("to-do-input");
+const addBtn = document.getElementById("add-btn");
 const toDoList = document.getElementById("cont-to-do-list");
-const conCompletList = document.getElementById("cont-complete");
+const contCompletedList = document.getElementById("cont-completed-list");
 
-//Creamos la funcion que nos permite crear una nueva tarea
 
-function createToDoItem(textoItem) {
-    //Creamos el nodo o elemento padre
+//CREAMOS LA FUNCION QUE NOS PERMITE CREAR UNA NUEVA TAREA APARTIR DEL FORMULARIO
+//TODA ETIQUETA QUE VAMOS A CREAR ES A PARTIR DE LA MAQUETA HTML PRE EXISTENTE
+
+
+//ESTA FUNCION CREA LA ESTRUCTURA DEL HTML Y LA DEJA EN UN LIMBO AUN NO LA INSERTA EN LA PAGINA
+
+function createToDoItem(textItem) {
+
+    //CREAMOS EL NODO O ELEMENTO PADRE
+
     const item = document.createElement("div");
-    //Toda etiqueta que vamos a crear es a partir de la maqueta html pre existente
     item.classList.add("item-to-do");
-    //Creamos el nodo o elemento hijo yle agregamos el nodo checkbox
+
+    //CREAMOS EL NODO HIJO DEL INPUT Y LE AGREGAMOS EL TYPE CHECKBOX
+
     const checkbox = document.createElement("input");
-    checkbox.type = ("checkbox");
-    //Creamos el siguiente nodo hijo, parrafo
-    //A este parrafo le asigno el valor del argumento de la funcion, 
-    //es decir lo que escribe el usuario en el campo
+    checkbox.type = "checkbox";
+
+
+    //CREAMOS EL SIGUIENTE NODO HIJO <p>
+    //A ESTE PARRAFO LE ASIGNO EL VALOR DEL ARGUMENTO DE LA FUNCION ES DECIR LO QUE ESCRIBE EL USUARIO EN EL CAMPO DEL FORMULARIO
+
     const p = document.createElement("p");
-    p.textContent = textoItem;
-    //Creamos el ultimo nodo hijo, el boton de eliminar
+    p.textContent = textItem
+
+    //CREAMOS EL ULTIMO NODO HIJO ,  EL BOTON DE ELIMINAR
+
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
-    deleteBtn.classList.add("delete-btn");
-    //Ensamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la tarea
+
+    //ENSAMBLAMOS DENTRO DEL NODO PADRE SUS NODOS HIJOS, ES DECIR LA ESTRUCTURA DE LA TAREA
+
     item.appendChild(checkbox);
     item.appendChild(p);
     item.appendChild(deleteBtn);
-    //Utilizamos el return para dar respuesta del elemento creado, ya que lo usaremos en otra funcion mas adelante
+
+    //UTILIZAMOS EL RETURN PARA DAR RESPUESTA DEL ELEMENTO CREADO YA QUE LO USAREMOS EN OTRA FUNCION MAS ADELANTE
+
     return item;
+
+
+
+
+
 }
 
-//Detectamos el click sobre el boton +, con un evento de escucha o listener
-//para que a partir de este evento se agregue la tarea, dentro del contenedor cont-to-do-list
-addBtn.addEventListener('click', ()=>{
-    const textoItem = input.value.trim();
-    if (textoItem == "") {
+
+//DETECTAMOS EL EVENTO CLICK SOBRE EL BOTON AGREGAR CON UN ELEMENTO DE ESCUCHA (listener) 
+//PARA QUE APARTIR DE ESTE EVENTO SE AGREGUE LA TAREA DENTRO DEL CONTENEDOR #cont-to-do-list
+
+addBtn.addEventListener('click', () => {
+
+    const textItem = input.value.trim();
+    if (textItem == "") {
+
+
         alert("No se puede crear una tarea vacia");
-    } else {
-        const newItem = createToDoItem(textoItem);
-        toDoList.appendChild(newItem);
-        input.value = "";
+
+
     }
+    else {
+
+        const newItem = createToDoItem(textItem);
+        toDoList.appendChild(newItem);
+        eventsToItem(newItem);
+        input.value = "";
+
+
+
+    }
+
 });
+
+//LA SIGUIENTE FUNCION NOS PERMITIRA AGREGAR EL FUNCIONAMIENTO PRINCIPAL DE LAS TAREAS ES DECIR MARCAR LA TAREA COMO COMPLETADA O EN DADO CASO ELIMINARLA
+
+
+function eventsToItem(item) {
+
+    //UTILIZAMOS querySelector PARA CAPTURAR EL INPUT Y BUTTON QUE ESTA DENTRO DEL ITEM
+
+    const checkbox = item.querySelector("input");
+    const deleteBtn = item.querySelector("button");
+
+    //COMPLETAR LA TAREA
+
+    checkbox.addEventListener('change', () => {
+
+        if (checkbox.checked) {
+            contCompletedList.appendChild(item);
+        }
+        else {
+            toDoList.appendChild(item);
+        }
+
+
+
+    })
+
+    deleteBtn.addEventListener('click', () => {
+
+        item.remove()
+
+    })
+
+
+}
+
+
+
